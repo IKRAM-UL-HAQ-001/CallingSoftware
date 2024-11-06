524dffa0a0e6d3bef7ee32f2887619680a71f8a0
IKRAM-UL-HAQ-001 <mrikramulhaq0005@gmail.com>
2024-11-07 01:06:23 +0500

    updated

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
     <!-- Introductory banner -->
     <div class="row">
         <div class="col-11 mb-xl-0 mx-auto my-5 border w-full bg-white rounded text-center d-flex justify-content-center align-items-center" style="height:200px;">
-            <h1 class="display-4 display-md-3 display-lg-2">Calling Management System</h1>        
+            <h1 class="display-4 display-md-3 display-lg-2">Call Center Software</h1>        
         </div>
     </div>
 
